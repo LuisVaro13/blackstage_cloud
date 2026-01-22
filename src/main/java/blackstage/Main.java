76a77c694ef638288ed7132b197f4c74f3453b43
LuisVaro13 <luisvaro.teleco@gmail.com>
2026-01-22 18:06:16 +0100

    Update Main.java

--- src/main/java/blackstage/Main.java
+++ src/main/java/blackstage/Main.java
@@ -1,45 +1,46 @@
 package blackstage;
 
-import static spark.Spark*;
+import  spark.Spark;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		// Configuración puerto
-		port(8080);
+		Spark.port(8080);
 
 		// Configuración del CORS
-		options("/*", (request, response) -> {
+		Spark.options("/*", (request, response) -> {
 			response.header("Access-Control-Allow-Headers", "*");
 			response.header("Access-Control-Allow-Methods", "*");
 			return "OK";
 		});
-		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+		Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 		
 		JsonTransformer jsonTransformer = new JsonTransformer();
 		
-		get("/productos", ProductoServices::getProductos, jsonTransformer);
-		get("/productos/:productoID", ProductoServices::getProducto, jsonTransformer);
+		Spark.get("/productos", ProductoServices::getProductos, jsonTransformer);
+		Spark.get("/productos/:productoID", ProductoServices::getProducto, jsonTransformer);
 		
-		get("/clientes", ClienteServices::loginCliente, jsonTransformer);
-		post("/clientes", ClienteServices::nuevoCliente, jsonTransformer);
-		put("/clientes/:clienteID", ClienteServices::modificarCliente, jsonTransformer);
-		delete("/clientes/:clienteID", ClienteServices::eliminarCliente, jsonTransformer);
+		Spark.get("/clientes", ClienteServices::loginCliente, jsonTransformer);
+		Spark.post("/clientes", ClienteServices::nuevoCliente, jsonTransformer);
+		Spark.put("/clientes/:clienteID", ClienteServices::modificarCliente, jsonTransformer);
+		Spark.delete("/clientes/:clienteID", ClienteServices::eliminarCliente, jsonTransformer);
 		
-		post("/clientes/:clienteID/pedidos", PedidoServices::nuevoPedido, jsonTransformer);
-		get("/clientes/:clienteID/pedidos", PedidoServices::mostrarPedidos, jsonTransformer);
-		get("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::mostrarPedido, jsonTransformer);
-		put("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::modificarPedido, jsonTransformer);
-		delete("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::eliminarPedido, jsonTransformer);
+		Spark.post("/clientes/:clienteID/pedidos", PedidoServices::nuevoPedido, jsonTransformer);
+		Spark.get("/clientes/:clienteID/pedidos", PedidoServices::mostrarPedidos, jsonTransformer);
+		Spark.get("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::mostrarPedido, jsonTransformer);
+		Spark.put("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::modificarPedido, jsonTransformer);
+		Spark.delete("/clientes/:clienteID/pedidos/:pedidoID", PedidoServices::eliminarPedido, jsonTransformer);
 		
-		post("/clientes/:clienteID/pedidos/:pedidoID/items", ItemServices::nuevoItem, jsonTransformer);
-		get("/clientes/:clienteID/pedidos/:pedidoID/items", ItemServices::mostrarItems, jsonTransformer);
-		put("/clientes/:clienteID/pedidos/:pedidoID/items/:itemID", ItemServices::modificarCantidad, jsonTransformer);
-		delete("/clientes/:clienteID/pedidos/:pedidoID/items/:itemID", ItemServices::eliminarItem, jsonTransformer);
+		Spark.post("/clientes/:clienteID/pedidos/:pedidoID/items", ItemServices::nuevoItem, jsonTransformer);
+		Spark.get("/clientes/:clienteID/pedidos/:pedidoID/items", ItemServices::mostrarItems, jsonTransformer);
+		Spark.put("/clientes/:clienteID/pedidos/:pedidoID/items/:itemID", ItemServices::modificarCantidad, jsonTransformer);
+		Spark.delete("/clientes/:clienteID/pedidos/:pedidoID/items/:itemID", ItemServices::eliminarItem, jsonTransformer);
 
 	}
 
 }
+
 
 
